166bad37ede33d6beaffbdb671f639d624399262
Johny Molano <molano.johny721@gmail.com>
2024-08-23 17:00:29 -0500

    Añadimos la funcion de que el al darle click a la imagen te redirija a el video en la plataforma

--- src/assets/main.js
+++ src/assets/main.js
@@ -21,16 +21,21 @@ async function fetchData(urlApi){
         let view = `
         ${videos.items.map(video =>`
             <div class="group relative">
+            <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
                 <div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    
                 </div>
                 <div class="mt-4 flex justify-between">
+                    
                     <h3 class="text-sm text-gray-700">
                         <span aria-hidden="true" class="absolute inset-0"></span>
                         ${video.snippet.title}
                     </h3>
                 </div>
+            </a>
             </div>
             
         `).slice(0,4).join('')}
